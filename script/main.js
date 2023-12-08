967281372be38077cf8b3f7347155c4eb1cf01b7
Eva Björling <eva.bjorling@chasacademy.se>
2023-12-08 09:33:37 +0100

    our first commit

---
+++ script/main.js
@@ -0,0 +1,16 @@
+/* 
+Ni väljer om ni använder produktdatan i js eller json och om ni vill lägga till 
+fler properties som bilder etc.
+
+Ni får också använda ett api som t.ex. https://fakestoreapi.com/ för att generara
+ut fiktiva produkter därifrån istället för från en lokal js/json.
+*/
+
+const products = [
+    { id: 1, name: 'T-shirt', category: 'kläder', price: 100 },
+    { id: 2, name: 'Hörlurar', category: 'elektronik', price: 250 },
+    { id: 3, name: 'Keps', category: 'kläder', price: 50 },
+    { id: 4, name: 'Mobiltelefon', category: 'elektronik', price: 500 }
+];
+
+const cart = [];
