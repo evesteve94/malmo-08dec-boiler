ce4a9e9b9f25e228a71f990c84421f841d7a9ebb
Eva Björling <eva.bjorling@chasacademy.se>
2023-12-08 09:54:58 +0100

    made first contact, displaying products/things in our HTML with map()

--- script/main.js
+++ script/main.js
@@ -6,7 +6,7 @@ Ni får också använda ett api som t.ex. https://fakestoreapi.com/ för att gen
 ut fiktiva produkter därifrån istället för från en lokal js/json.
 */
 
-const products = [
+const things = [
     { id: 1, name: 'T-shirt', category: 'kläder', price: 100 },
     { id: 2, name: 'Hörlurar', category: 'elektronik', price: 250 },
     { id: 3, name: 'Keps', category: 'kläder', price: 50 },
@@ -14,3 +14,18 @@ const products = [
 ];
 
 const cart = [];
+
+
+//visa våra produkter - bara namn och pris
+let productResults = things.map(thing => {
+    return `
+    <div class= "product-card">
+    <h3> ${thing.name} </h3>
+    <h4> ${thing.price} </h4>
+    </div>   `
+})
+//koppla till product-container
+let productContainer = document.getElementById('product-container');
+
+//lägga till i vår div
+productContainer.innerHTML = productResults.join('');
